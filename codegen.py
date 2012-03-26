0b46d572569356c12457baab755ce927b1bd2906
Gordon Potter <gordon@gordonpotter.com>
2012-03-26 00:01:41 -0700

    logic for single and multi file output .h and .c TODO: Refactor to deal with general file types

--- codegen.py
+++ codegen.py
@@ -1,68 +1,66 @@
+import sys
 from lib import F_Helpers
 from lib import T_Header
 from lib import T_Implementation
 
 
-# # print T_Header.getCodePatternHeader("myType", "myGetter", "mySetter")
-# 
-# params = {'typeDef':'myType', 'getterName':'myGetter', 'setterName':'mySetter', 'cacheVariable':'_cacheVar', 'defaultValue':'0', 'returnValue':'TRUE' }
-# 
-# # print T_Implementation.getCodePatternSynthesize(params)
-# 
-# params = {
-# 'paramName':'myParam', 
-# 'typeDef':'myType', 
-# 'getterName':'myGetter', 
-# 'setterName':'mySetter', 
-# 'cacheVariable':'_cacheVar', 
-# 'defaultValue':'0', 
-# 'returnValue':'TRUE'
-# }
-# 
-# paramsEmpty = {}
-# 
-# print T_Implementation.getCodePatternSynthesize(params)
-# 
-# print T_Implementation.getCodePatternSynthesize(paramsEmpty)
+if len(sys.argv) < 3:
+    sys.exit('Usage: python %s [out_file] [inputs_file] [is_single_file=True]' % sys.argv[0])
 
-# params = {'typeDef':'myType', 'getterName':'myGetter', 'setterName':'mySetter', 'cacheVariable':'_cacheVar', 'defaultValue':'0', 'returnValue':'TRUE' }
 
-params = {
-'paramName':'myParam', 
-'typeDef':'myType', 
-'getterName':'myGetter', 
-'setterName':'mySetter', 
-'cacheVariable':'_cacheVar', 
-'defaultValue':'0', 
-'returnValue':'TRUE'
-}
+out_file = sys.argv[1]
+inputs_file = "inputs/" + sys.argv[2]
+isSingleFile = sys.argv[3]
 
-codeOutput = ''
-codeOutput += T_Header.getCodePatternHeader(params)
-codeOutput += T_Implementation.getCodePatternSynthesize(params)
+print isSingleFile
 
-print T_Header.getCodePatternHeader(params)
-print T_Implementation.getCodePatternSynthesize(params)
+myList = F_Helpers.parseTextFile(inputs_file)
 
-fileName = "code.c"
-F_Helpers.writeTemplateFile(fileName, codeOutput)
+if isSingleFile == "True":
+    print "Is single file output using the following parameters"
+    print myList
 
-# inFile = "inputs/input_text.txt"
-# myList = F_Helpers.parseTextFile(inFile)
-# 
-# print myList
-# 
-# from inputs import input_code
-# 
-# myData = input_code.inputData
-# 
-# # inFileCode = "input/input_code.py"
-# # myData = F_Helpers.loadCodeFromFile(inFileCode)
-# 
-# for item in myData:
-#     print item
-# 
-# for item in myList:
-#     print item
-# # print myData[2]
+headerData = ''
+codeData = ''
 
+for item in myList:
+    paramName = item[1]
+    typeDef = item[0]
+    getterName = "get" + item[1]
+    setterName = "set" + item[1]
+    cacheVariable = "_cache" + item[1]
+    if typeDef == "char *":
+        defaultValue = "0\0"
+    elif typeDef == "int":
+        defaultValue = "0"
+    else:
+        defaultValue = "{0}"
+    returnValue = "TRUE"
+    params = {
+    'paramName':paramName, 
+    'typeDef':typeDef, 
+    'getterName':getterName, 
+    'setterName':setterName, 
+    'cacheVariable':cacheVariable, 
+    'defaultValue':defaultValue, 
+    'returnValue':returnValue
+    }
+
+    if isSingleFile == "True":
+        headerData += T_Header.getCodePatternHeader(params)
+        codeData += T_Implementation.getCodePatternSynthesize(params)
+    else:
+        headerData = T_Header.getCodePatternHeader(params)
+        fileName = paramName + ".h"
+        F_Helpers.writeTemplateFile(fileName, headerData)
+
+        codeData = T_Implementation.getCodePatternSynthesize(params)
+        fileName = paramName + ".c"
+        F_Helpers.writeTemplateFile(fileName, codeData)
+
+if isSingleFile == "True":
+    fileName = out_file + ".h"
+    F_Helpers.writeTemplateFile(fileName, headerData)
+
+    fileName = out_file + ".c"
+    F_Helpers.writeTemplateFile(fileName, codeData)
